614cb34f72870055aa502f8324b9e238f76f238a
siriketuanal-cpu <siriketuanal@gmail.com>
2026-09-02 20:01:32 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'nyanko-split-v510';
+const C = 'nyanko-split-v511';
 const A = [
   './', './index.html', './update.html', './core.mjs', './store.mjs',
   './manifest.json', './icon.svg',
